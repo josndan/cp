b9f8c106188c5240610490041db834f508b09706
Joseph Selvaraaj <jdselvaraaj@gmail.com>
2024-10-12 08:59:54 -0400

    Setup

---
+++ cmake-build-debug/CMakeFiles/cp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cp.dir/main.cpp.o"
+  "CMakeFiles/cp.dir/main.cpp.o.d"
+  "cp"
+  "cp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
